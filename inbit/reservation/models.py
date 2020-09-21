e51c82e1a4106504dedaec003b1cd27a79b8635f
seojinsarejj <s19009@naver.com>
2020-09-21 20:01:38 +0900

    [CREATE] Create Queue,Reservation view

--- inbit/reservation/models.py
+++ inbit/reservation/models.py
@@ -4,20 +4,21 @@ from django.db import models
 
 
 class Reservation(models.Model):
-    #room_id = models.ForeignKey("room.Room",on_delete=models.CASCADE)
+    room_id = models.ForeignKey("room.Room",on_delete=models.CASCADE)
     #email = models.ForeignKey("user.User",on_delete=models.CASCADE)
+    email = models.CharField(max_length=50)
     date = models.DateField(auto_now_add=True)
     period = models.IntegerField()
 
 
 class Reservation_queue(models.Model):
-    #room_id = models.ForeignKey("room.Room",on_delete=models.CASCADE)
+    room_id = models.ForeignKey("room.Room",on_delete=models.CASCADE)
     #email = models.ForeignKey("user.User",on_delete=models.CASCADE)
+    email = models.CharField(max_length=50)
     content = models.TextField(max_length=255)
     period = models.IntegerField()
     timestamp = models.DateTimeField(auto_now_add=True)
     status = models.BooleanField(default=True)
-
 
 
 
--- inbit/reservation/views.py
+++ inbit/reservation/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import generics
+from rest_framework import generics,mixins,viewsets
 
 from .models import Reservation,Reservation_queue
 from .serializers import ReservationSerializer, QueueSerializer
@@ -12,29 +12,36 @@ class CreateQueueAPI(generics.CreatedAPIView):
     #인증검사
     serializer_class = QueueSerializer
 
+    #email -> self.request.user.email 로 바꿀것
     def post(self, request, *arg, **kwargs):
-        request.data["email"] = self.request.user.email
+        request.data["email"] = "email"
         return super().post(request,*args,**kwargs)
         
 
-class GetReservationAPI(generics.RetrieveAPIView):
+    
+class ListReservationAPI(generics.ListAPIView):
 
-    #인증검사
     serializer_class = ReservationSerializer
 
     def get(self,request,*arg,**kwargs):
         return super().get(request,*arg,**kwargs)
 
-    def get_object(self):
-        obj = Reservation.object.get(id=self.kwargs["id"])
-        return obj
+    def get_queryset(self):
+        return Reservation.objects.all()
     
 
-class AdminQueueAPI(generics.RetrieveUpdateDestroyAPIView):
-
-    #인증검사
+class AdminQueueAPI(viewsets.ModelViewSet):
     serializer_class = QueueSerializer
+    queryset = Reservation_queue.objects.all()
+
+    #email -> self.request.user.email 로 바꿀것
+    def post(self,request,*arg,**kwargs):
+        request.data["email"] = "email"
+        return super().post(reqeust,*args,**kwargs)
+    
 
 
-class AdminReservationAPI(generics.)
+class AdminReservationAPI(generics.Model):
+    serializer_class = ReservationSerializer
+    queryset = Reservation.objects.all()
     
