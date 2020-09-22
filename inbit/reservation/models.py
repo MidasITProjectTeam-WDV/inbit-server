c1fab587dcbfed703cd501c5b5050f0499e19f1a
seojinsarejj <s19009@naver.com>
2020-09-22 14:15:18 +0900

    [CREATE] Create GetReservationAPI

--- inbit/reservation/models.py
+++ inbit/reservation/models.py
@@ -5,20 +5,19 @@ from django.db import models
 
 class Reservation(models.Model):
     room_id = models.ForeignKey("room.Room",on_delete=models.CASCADE)
-    #email = models.ForeignKey("user.User",on_delete=models.CASCADE)
-    email = models.CharField(max_length=50)
+    email = models.ForeignKey("user.User",on_delete=models.CASCADE)
     date = models.DateField(auto_now_add=True)
     period = models.IntegerField()
 
 
 class Reservation_queue(models.Model):
     room_id = models.ForeignKey("room.Room",on_delete=models.CASCADE)
-    #email = models.ForeignKey("user.User",on_delete=models.CASCADE)
-    email = models.CharField(max_length=50)
+    email = models.ForeignKey("user.User",on_delete=models.CASCADE)
     content = models.TextField(max_length=255)
     period = models.IntegerField()
     timestamp = models.DateTimeField(auto_now_add=True)
-    status = models.BooleanField(default=True)
+    status = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
 
 
 
--- inbit/reservation/views.py
+++ inbit/reservation/views.py
@@ -4,10 +4,19 @@ from rest_framework.response import Response
 from rest_framework import generics,mixins,viewsets
 from .models import Reservation,Reservation_queue
 from .serializers import ReservationSerializer, QueueSerializer
+from rest_framework.permissions import IsAuthenticated
+from user.permissions import IsAdmin
+from rest_framework_jwt.authentication import JSONWebTokenAuthentication
+from rest_framework.decorators import action
+
+import datetime
 # Create your views here.
 
 class QueueViewSet(viewsets.ViewSet):
-    #인증검사
+    
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [JSONWebTokenAuthentication]
+
     def create(self,request):
         request.data["email"] = request.user.email
         serializer = Reservation_queue(data=request.data)
@@ -20,8 +29,8 @@ class QueueViewSet(viewsets.ViewSet):
 
 
     def list(self, request):
-
-        queryset = Reservation_queue.objects.filter(email=request.user.email)
+        today = datetime.datetime.now().strftime('%Y-%m-%d')
+        queryset = Reservation_queue.objects.filter(email=request.user.email).filter(date=today)
         serializer = QueueSerializer(queryset, many=True)
 
         return Response(serializer.data)
@@ -52,8 +61,12 @@ class QueueViewSet(viewsets.ViewSet):
 
 class GETReservationAPI(generics.GenericAPIView,mixins.RetrieveModelMixin):
 
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [JSONWebTokenAuthentication]
+
     def retrieve(self, request, *args, **kwargs):
-        room_queryset = Reservation.objects.get(room_id=self.kwargs["room_id"])
+        today = datetime.datetime.now().strftime('%Y-%m-%d')
+        room_queryset = Reservation.objects.get(room_id=self.kwargs["room_id"]).filter(date=today)
         first_queryset = list(room_queryset.objects.filter(period=10))
         second_queryset = list(room_queryset.objects.filter(period=11))
         
@@ -72,20 +85,54 @@ class GETReservationAPI(generics.GenericAPIView,mixins.RetrieveModelMixin):
 
         
 
-
-
 class AdminQueueAPI(viewsets.ModelViewSet):
+
+    permission_classes = [IsAuthenticated & IsAdmin]
+    authentication_classes = [JSONWebTokenAuthentication]
     serializer_class = QueueSerializer
     queryset = Reservation_queue.objects.all()
 
-    #email -> self.request.user.email 로 바꿀것
-    def post(self,request,*arg,**kwargs):
-        request.data["email"] = "email"
-        return super().post(request,*arg,**kwargs)
+    def list(self, request):
+        queryset = Reservation_queue.objects.filter(is_active=True)
+        serializer = QueueSerializer(queryset, many=True)
+
+        return Response(serializer.data)
+    
+    def post(self,request,*args,**kwargs):
+        request.data["email"] = self.request.user.email
+        return super().post(request,*args,**kwargs)
+
+    
+    @action(detail=True,methods=['PATCH'])
+    def accept(self,request,*args,**kwargs):  
+        kwargs['partial'] = True
+        obj = Reservation_queue.objects.filter(pk=kwargs["pk"])
+        data = {
+            "room_id":obj["room_id"],
+            "email":obj["email"],
+            "period":obj["period"]
+        }
+        serializer = ReservationSerializer(data=data)
+
+        if not serializer.is_valid():
+            return Response(serializer.errors)
+
+        serializer.save()
+
+        return self.update(request, *args, **kwargs)
+        
+
+    @action(detail=True,methods=['PATCH'])
+    def refuse(self,request,*args,**kwargs):
+        kwargs['partial'] = True
+        return self.update(request, *args, **kwargs)
+ 
     
 
 
 class AdminReservationAPI(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated & IsAdmin]
+    authentication_classes = [JSONWebTokenAuthentication]
     serializer_class = ReservationSerializer
     queryset = Reservation.objects.all()
     
