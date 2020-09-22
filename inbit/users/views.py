856add283d08a383872da84484ef0e69a2f61ac6
ByunWoungSub <50494659+ByunWoungSub@users.noreply.github.com>
2020-09-22 09:18:59 +0900

    [UPDATE] views

--- inbit/users/views.py
+++ inbit/users/views.py
@@ -1,5 +1,6 @@
 import json
 import jwt
+import bcrypt
 from django.views import View
 from django.http import JsonResponse
 from django.conf import settings
@@ -24,6 +25,9 @@ class SignUpView(View):
             response = JsonResponse({"message":"already existed email"}, status = 401)
             return response
         else:
+            # password = data["password"].encode('utf-8')
+            # password_crypt = bcrypt.hashpw(password, bcrypt.gensalt())
+            # password_crypt = password_crypt.decode('utf-8')
             Users.objects.create(email = data["email"], name = data["name"], password = data["password"], phone_num = data["phone_num"], position = data["position"],)
             response =  JsonResponse({"message":"successfully registered"}, status = 200)
             return response
@@ -35,10 +39,10 @@ class LoginView(View):
         try:    
             if Users.objects.filter(email = data["email"]).exists():
                 user = Users.objects.get(email= data["email"])
-                if Users.objects.filter(password = data["password"]):
+                if Users.objects.get(email=data["email"], password=data["password"]):
                     token = jwt.encode({'email': data['email']}, settings.JWT_SECRET_KEY, algorithm=settings.JWT_ALGORITHM)
                     token = token.decode('utf-8')
-                    response = JsonResponse({"message": "login success","token":token, "email":data["email"]}, status=200)
+                    response = JsonResponse({"message": "login success", "token": token, "email": data["email"]}, status=200)
                     return response
                 else:
                     response = JsonResponse({"message":"password not matched"})
@@ -53,6 +57,6 @@ class LoginView(View):
 
 
 class LogoutView(View):
-    def post(self, request):
+    def delete(self, request):
         request.user.auth_token.delete()
         return JsonResponse(status=status.HTTP_200_OK)
